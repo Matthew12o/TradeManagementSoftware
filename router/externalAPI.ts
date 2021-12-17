a04e79fb9c2b9b646e45996a8a9e892676e00156
Matthew12o <72840122+Matthew12o@users.noreply.github.com>
2021-12-17 00:31:38 -0500

    Update externalAPI.ts

--- router/externalAPI.ts
+++ router/externalAPI.ts
@@ -1,6 +1,6 @@
 import express from 'express';
-import request from 'request-promise';
-import request_2 from 'request';
+//import request from 'request-promise';
+import request from 'request';
 
 const externalAPIRouter = express.Router();
 
@@ -10,47 +10,26 @@ const IBAPI_baseurl = 'https://localhost:8080/v1/api'
 
 // Test Script
 externalAPIRouter.get('/TEST', (req, res, next) => {
-    request_2({
+    request({
         uri: 'https://ghibliapi.herokuapp.com/films',
+        method: 'GET'
     }).pipe(res);
 });
 
 // IB Session Logic
 externalAPIRouter.get('/IB/Session/Validate', (req, res, next) => {
     const api_url = `${IBAPI_baseurl}/sso/validate`;
-    var options = {
-        method: 'GET',
-        url: api_url,
-        json: true,
-    }
-    request(options)
-        .then((parsedBody) => {
-            // Do something
-
-        })
-        .catch((err)=>{
-
-        })
-    
+    request(api_url, {
+        method: 'GET'
+    }).pipe(res);
     next();
-
 });
 
 externalAPIRouter.get('/IB/Session/AuthenticateStatus', (req, res, next) => {
     const api_url = `${IBAPI_baseurl}/iserver/auth/status`;
-    var options = {
-        method: 'POST',
-        url: api_url,
-        json: true,
-    }
-    request(options)
-        .then((parseBody) => {
-            // Do something
-        })
-        .catch((err) => {
-
-        })
-
+    request(api_url, {
+        method: 'POST'
+    }).pipe(res);
     next();
 });
 
@@ -61,34 +40,13 @@ externalAPIRouter.all('/IB/Ping', (req, res, next) => {
         uri: api_url,
         json: true,
     }
-    request(options)
-        .then((parseBody) => {
-            // Do something 
-            console.log(parseBody);
-        })
-        .catch((err) => {
-
-        })
-
+    request(api_url, {method: 'POST'}).pipe(res);
     next();
 });
 
-
 externalAPIRouter.get('/IB/Logout', (req, res, next) => {
     const api_url = `${IBAPI_baseurl}/logout`;
-    var options = {
-        method: 'POST',
-        url: api_url,
-        json: true
-    }
-    request(options)
-        .then((parseBody) => {
-            // Do something
-        })
-        .catch((err) => {
-
-        })
-
+    request(api_url, {method: 'POST'}).pipe(res);
     next();
 });
 // Bloomberg API
