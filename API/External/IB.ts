9c3dfc06604841c3c5754e2784ecf7aea854dfd8
Matthew12o <72840122+Matthew12o@users.noreply.github.com>
2021-12-18 17:40:04 -0500

    Update IB.ts

--- API/External/IB.ts
+++ API/External/IB.ts
@@ -242,6 +242,9 @@ class Contract {
     private static url_futures_by_symbol = '/trsrv/futures';
     private static url_stock_by_symbol = '/trsrv/stocks';
     private static url_search_symbol = '/iserver/secdef/search';
+    private static url_search_strike = '/iserver/secdef/strikes';
+    private static url_secdef_info = '/iserver/secdef/info';
+    private static url_contract_rules = '/iserver/contract/rules';
 
     url: string;
     constructor(baseURL: string) {
@@ -249,14 +252,14 @@ class Contract {
     }
 
     Secdef = async (contract_id: [any]) => {
-        const payload = {'body' : {'conids' : contract_id}};
+        const payload = {body : {conids : contract_id}};
         return await postRequest(`${this.url}${Contract.url_secdec_by_conid}`, payload);
     };
 
     TradingSchedule = async (asset_class: string, symbol: string, exchange?: string, exchange_filter?: string) => {
         let initial_payload = {
-            'assetClass' : asset_class,
-            'symbol' : symbol
+            assetClass : asset_class,
+            symbol : symbol
         };
         if (typeof exchange !== 'undefined') {
             initial_payload = {...initial_payload, ...{'exchange' : exchange}}
@@ -275,7 +278,7 @@ class Contract {
         } else {
             single_string_symbol = symbols;
         }
-        const payload = { body: {'symbols' : single_string_symbol} };
+        const payload = { body: {symbols : single_string_symbol} };
         return await getRequest(`${this.url}${Contract.url_futures_by_symbol}`, payload);
     };
 
@@ -286,7 +289,7 @@ class Contract {
         } else {
             single_string_symbol = symbols;
         }
-        const payload = { body: {'symbols' : single_string_symbol}};
+        const payload = { body: {symbols : single_string_symbol}};
         return await getRequest(`${this.url}${Contract.url_stock_by_symbol}`, payload);
     }
 
@@ -297,17 +300,86 @@ class Contract {
     };
 
     SearchBySymbol = async (symbol: string, name?: boolean, secType?: string) => {
-        let initial_payload = { 'symbol' : symbol };
+        let initial_payload = { symbol : symbol };
         if (typeof name !== 'undefined') {
-            initial_payload = {...initial_payload, ...{'name' : name}};
+            initial_payload = {...initial_payload, ...{name : name}};
         };
         if (typeof secType !== 'undefined') {
-            initial_payload = {...initial_payload, ...{'secType' : secType}};
+            initial_payload = {...initial_payload, ...{secType : secType}};
         };
         const payload = { body: initial_payload };
         return await postRequest(`${this.url}${Contract.url_search_symbol}`, payload);
 
     }
+
+    SearchStrike = async (contract_id: string, sectype: string, month: string, exchange?: string) => {
+        let initial_payload = {
+            conid : contract_id,
+            sectype : sectype,
+            month : month
+        }
+        if (typeof exchange !== 'undefined') {
+            initial_payload = {...initial_payload, ...{exchange: exchange}};
+        }
+        const payload = { body: initial_payload };
+        return await getRequest(`${this.url}${Contract.url_search_strike}`);
+    }
+
+    SecdefInfo = async (contract_id: string, sectype: string, month?: string, exchange?: string, strike?: string, right?: string) => {
+        let initial_payload = {
+            conid: contract_id,
+            sectype: sectype
+        }
+        if (typeof month !== 'undefined') {
+            initial_payload = {...initial_payload, ...{month : month}};
+        }
+        if (typeof exchange !== 'undefined') {
+            initial_payload = {...initial_payload, ...{exchange : exchange}};
+        }
+        if (typeof strike !== 'undefined') {
+            initial_payload = {...initial_payload, ...{strike : strike}};
+        }
+        if (typeof right !== 'undefined') {
+            initial_payload = {...initial_payload, ...{right : right}};
+        }
+        const payload = {body: initial_payload};
+        return await getRequest(`${this.url}${Contract.url_secdef_info}`, payload);
+    };
+
+    IBAlgoParams = async (contract_id: string, algos?: string, addDescription?: string, addParams?: string) => {
+        const url = `/iserver/contract/${}/algos`;
+        let initial_payload = {};
+        if (typeof algo !== 'undefined') {
+            initial_payload = {...initial_payload, ...{algos : algos}}
+        }
+        if (typeof addDescription !== 'undefined') {
+            initial_payload = {...initial_payload, ...{addDescription : addDescription}};
+        }
+        if (typeof addParams !== 'undefined') {
+            initial_payload = {...initial_payload, ...{ addParams : addParams }};
+        }
+        const payload = {body: initial_payload};
+        return await getRequest(`${this.url}${url}`, payload);
+    };
+
+    ContractRules = async (contract_id: string, isBuy: boolean) => {
+        let initial_payload = {
+            conid : contract_id,
+            isBuy : isBuy
+        }
+        const payload = { body : initial_payload };
+        return await postRequest(`${this.url}${Contract.url_contract_rules}`);
+    };
+
+    ContractInfoAndRules = async (contract_id: string, isBuy: boolean) => {
+        const url = `/iserver/contract/${contract_id}/info-and-rules`;
+        let initial_payload = {
+            isBuy : isBuy
+        }
+        const payload = { body: initial_payload };
+        return await getRequest(`${this.url}${url}`, payload);
+    }
+
 }
 
 export {IB_API}
